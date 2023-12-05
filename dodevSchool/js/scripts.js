8d04246b720b6756a95340191d21611858ba6d02
Luiz Fernando Dias Costa <luizfdias277@gmail.com>
2023-12-05 10:23:27 -0300

    adicionando exercício entregavel sistema alunos

--- dodevSchool/js/scripts.js
+++ dodevSchool/js/scripts.js
@@ -3,32 +3,68 @@
 ////////////////////////////////////////////////////////////////////////
 
 class Aluno {
+  Nome
+  Idade
+  Nota
+  
+  constructor(nome, idade, nota) {
+    this.Nome = nome
+    this.Idade = idade
+    this.Nota = nota
+  }
   
 }
 
 // Array
-
+let arrayAlunos = []
 
 //funções projeto
 
-function CadastrarAluno() {
-  
+function CadastrarAluno(nome, idade, nota, array) {
+  let Alunos = new Aluno(nome, idade, nota)
+  if (!array.some(x => x.Nome == nome))
+    array.push(Alunos)
+  return Alunos
 }
 
-function OrdenarPorNota() {
- 
+function OrdenarPorNota(array) {
+  array.sort((a, b) => a.Nota - b.Nota)
+  return array
 }
 
-function OrdenarPorIdade() {
-
+function OrdenarPorIdade(array) {
+  array.sort((a, b) => b.Idade - a.Idade)
+  return array
 }
 
 function OrdenarPorNome() {
+  array.sort((a, b) => {
+    const primeiroNome = a.Nome.toUpperCase();
+    const segundoNome = b.Nome.toUpperCase();
 
+    if (primeiroNome < segundoNome) {
+      return -1;
+    }
+    if (primeiroNome > segundoNome) {
+      return 1;
+    }
+    return 0;
+  });
+  return array;
 }
 
 function CalcularMedia(){
+  if (array.length === 0) {
+    return 0;
+  }
+  let somaNotas = 0;
 
+  array.forEach((aluno) => {
+    somaNotas += Number(aluno.Nota)
+  });
+
+  const media = somaNotas / array.length;
+  return media;
 }
 
 ////////////////////////////////////////////////////////////////////////
